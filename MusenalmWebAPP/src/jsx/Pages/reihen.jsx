d0a5cbd9f18d31f71de96eb51a0239e20f4c8b99
Simon Martens <schnulller@mailbox.org>
2023-08-16 07:53:32 +0200

    Neue Daten

--- MusenalmWebAPP/src/jsx/Pages/reihen.jsx
+++ MusenalmWebAPP/src/jsx/Pages/reihen.jsx
@@ -57,12 +57,26 @@ function Reihe(props) {
                 </td>
                 </>
             )}>
-                <td colspan="2" class="px-1.5 py-0.5 bg-indigo-100 border-indigo-200 border-2">
+                <td colspan="2" class="px-1.5 py-0.5 pb-2 bg-indigo-100 border-indigo-200 border-2">
                     {props.reihe.sortiername}
                     <div>
                     <Show when={d()} fallback={<p>Daten werden geladen...</p>}>
+                    <div class="grid grid-cols-[5%_1fr_5%] gap-x-2 px-2 py-1 mb-0.5">
+                        <div>
+                            ID
+                        </div>
+                        <div>
+                            Titel
+                        </div>
+                        <div class="text-right">
+                            Jahr
+                        </div>
+                    </div>
                         <For each={d()}>{(band) =>
-                            <div class="grid grid-cols-[90%_1fr] gap-y-1 gap-x-2 px-2 py-1 my-2 border border-indigo-200 bg-indigo-50">
+                            <div class="grid grid-cols-[5%_1fr_5%] gap-y-1 gap-x-2 px-2 py-1 border border-indigo-200 bg-indigo-50 mb-1">
+                            <div>
+                                {band.id}
+                            </div>
                             <div>
                                 {band.titelTranskription}
                             </div>
