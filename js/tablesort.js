ddf9a468f41de7c4444c74586e30d64c14bfe8a3
Simon Martens <simonmartens@mailbox.org>
2023-09-05 20:56:16 +0200

    Deployed 3aead67 with MkDocs version: 1.5.2

--- js/tablesort.js
+++ js/tablesort.js
@@ -1,5 +1,5 @@
 document$.subscribe(function() {
-    var tables = document.querySelectorAll("article table:not([class])")
+    var tables = document.querySelectorAll("article .sortable-table+div table:not([class])")
     tables.forEach(function(table) {
         var th = table.querySelector("th");
         if (th !== null) {
